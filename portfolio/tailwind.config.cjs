89c47703171194607207cfc432139217089ff1f0
Lucas Fonseca <lfonseca010@gmail.com>
2025-03-31 21:44:34 -0300

    Basic setup

---
+++ portfolio/tailwind.config.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  darkMode: 'class',
+  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      animation: {
+        fadeIn: 'fadeIn 1s ease-in-out',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
