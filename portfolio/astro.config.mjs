f5f17347f169653bd93b1173e102a32845bb5b9e
Lucas Fonseca <lfonseca010@gmail.com>
2025-04-14 22:20:14 -0300

    Configs and testing

--- portfolio/astro.config.mjs
+++ portfolio/astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  base: "/lucasl2f.github.io/",
   vite: {
     plugins: [tailwindcss()],
   },
