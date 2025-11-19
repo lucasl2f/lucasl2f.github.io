492bf33600dce3d16dfda6f7804576fe153f3886
Lucas Fonseca <lfonseca010@gmail.com>
2025-11-19 18:43:27 -0300

    Add content

---
+++ src/pages/projects/games/crafting-back-home.md
@@ -0,0 +1,19 @@
+---
+layout:  /src/layouts/MovieLayout.astro
+title: 'Crafting Back Home'
+pubDate: 2025-11-15
+description: 'Game'
+languages: ["godot"]
+youtubeLink: 'https://www.youtube.com/watch?v=1OChBZ7sBtU'
+image:
+  url: ""
+  alt: "Thumbnail of Crafting Back Home game."
+---
+
+**Crafting Back Home** is a small time-travel crafting where you explore, collect materials, and combine them until you build the final piece.
+
+Play it on Itch.io: https://metal2f.itch.io/crafting-back-home
+
+## 🧩 Rule
+
+- Solo indie developer
--- src/utils/languages.ts
+++ src/utils/languages.ts
@@ -1,84 +1,88 @@
 export interface Language {
-  name: string;
-  iconName: string;
-  className?: string;
+    name: string;
+    iconName: string;
+    className?: string;
 }
 
 export const languages: Record<string, Language> = {
-  unity: {
-    name: "Unity",
-    iconName: "devicon:unity",
-  },
-  blender: {
-    name: "Blender",
-    iconName: "devicon:blender",
-  },
-  photoshop: {
-    name: "Photoshop",
-    iconName: "devicon:photoshop",
-  },
-  gimp: {
-    name: "Gimp",
-    iconName: "devicon:gimp",
-  },
-  csharp: {
-    name: "C#",
-    iconName: "devicon:csharp",
-  },
-  git: {
-    name: "Git",
-    iconName: "devicon:git",
-  },
-  premiere: {
-    name: "Premiere",
-    iconName: "skill-icons:premiere",
-  },
-  after: {
-    name: "After Effects",
-    iconName: "skill-icons:aftereffects",
-  },
-  linux: {
-    name: "Linux",
-    iconName: "devicon:linux",
-  },
-  astro: {
-    name: "Astro",
-    iconName: "devicon:astro",
-  },
-  html: {
-    name: "HTML 5",
-    iconName: "devicon:html5",
-  },
-  javascript: {
-    name: "JavaScript",
-    iconName: "devicon:javascript",
-  },
-  tailwind: {
-    name: "Tailwind CSS",
-    iconName: "devicon:tailwindcss",
-  },
-  figma: {
-    name: "Figma",
-    iconName: "devicon:figma",
-  },
-  markdown: {
-    name: "Markdown",
-    iconName: "devicon:markdown",
-  },
-  css: {
-    name: "CSS",
-    iconName: "devicon:css3",
-  },
-  gatsby: {
-    name: "Gatsby",
-    iconName: "devicon:gatsby",
-  },
-  python: {
-    name: "Python",
-    iconName: "devicon:python",
-  },
+    unity: {
+        name: "Unity",
+        iconName: "devicon:unity",
+    },
+    godot: {
+        name: "Godot",
+        iconName: "devicon:godot",
+    },
+    blender: {
+        name: "Blender",
+        iconName: "devicon:blender",
+    },
+    photoshop: {
+        name: "Photoshop",
+        iconName: "devicon:photoshop",
+    },
+    gimp: {
+        name: "Gimp",
+        iconName: "devicon:gimp",
+    },
+    csharp: {
+        name: "C#",
+        iconName: "devicon:csharp",
+    },
+    git: {
+        name: "Git",
+        iconName: "devicon:git",
+    },
+    premiere: {
+        name: "Premiere",
+        iconName: "skill-icons:premiere",
+    },
+    after: {
+        name: "After Effects",
+        iconName: "skill-icons:aftereffects",
+    },
+    linux: {
+        name: "Linux",
+        iconName: "devicon:linux",
+    },
+    astro: {
+        name: "Astro",
+        iconName: "devicon:astro",
+    },
+    html: {
+        name: "HTML 5",
+        iconName: "devicon:html5",
+    },
+    javascript: {
+        name: "JavaScript",
+        iconName: "devicon:javascript",
+    },
+    tailwind: {
+        name: "Tailwind CSS",
+        iconName: "devicon:tailwindcss",
+    },
+    figma: {
+        name: "Figma",
+        iconName: "devicon:figma",
+    },
+    markdown: {
+        name: "Markdown",
+        iconName: "devicon:markdown",
+    },
+    css: {
+        name: "CSS",
+        iconName: "devicon:css3",
+    },
+    gatsby: {
+        name: "Gatsby",
+        iconName: "devicon:gatsby",
+    },
+    python: {
+        name: "Python",
+        iconName: "devicon:python",
+    },
 };
 
 export const getLanguage = (lang: string): Language => {
-  return languages[lang] || languages.html;
+    return languages[lang] || languages.html;
 };
